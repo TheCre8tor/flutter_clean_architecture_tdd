d791d2032a13d09df34c5d6d3c82d5952b6f581e
Alexander Nitiola <cre8tor.alexander@gmail.com>
2022-02-17 15:55:09 +0100

    Updated gradle dependency

---
+++ lib/features/number_trivia/presentation/pages/number_trivia_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class NumberTriviaPage extends StatelessWidget {
+  const NumberTriviaPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Number Trivia"),
+      ),
+    );
+  }
+}
--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,10 +1,10 @@
-import 'package:clean_architecture_and_tdd/core/network/network_info.dart';
-import 'package:clean_architecture_and_tdd/core/utils/input_converter.dart';
-import 'package:clean_architecture_and_tdd/features/number_trivia/data/repositories/number_trivia_repository_impl.dart';
-import 'package:clean_architecture_and_tdd/features/number_trivia/domain/repositories/number_trivia_repository.dart';
-import 'package:clean_architecture_and_tdd/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
-import 'package:clean_architecture_and_tdd/features/number_trivia/domain/usecases/get_random_number_trivia.dart';
-import 'package:clean_architecture_and_tdd/features/number_trivia/presentation/bloc/number_trivia_bloc.dart';
+import 'core/network/network_info.dart';
+import 'core/utils/input_converter.dart';
+import 'features/number_trivia/data/repositories/number_trivia_repository_impl.dart';
+import 'features/number_trivia/domain/repositories/number_trivia_repository.dart';
+import 'features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
+import 'features/number_trivia/domain/usecases/get_random_number_trivia.dart';
+import 'features/number_trivia/presentation/bloc/number_trivia_bloc.dart';
 import 'package:get_it/get_it.dart';
 import 'package:http/http.dart';
 import 'package:internet_connection_checker/internet_connection_checker.dart';
